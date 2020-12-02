040052122ce7a2d72c52fcb291db370536d60451
aleks-lyashenko <74264242+aleks-lyashenko@users.noreply.github.com>
2020-12-02 15:26:57 +0300

    Add files via upload

---
+++ test/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1000,
+    auto: true,
+    adaptiveHeight: true
+
+  });
+});
+
+$( function() {
+    $( "#menu" ).menu();
+  } );
